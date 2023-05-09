64d10afd7a7d4829459d2010369213bb2ce155a5
Mohd <mwajahathali29@massmutual.com>
2023-05-09 12:36:28 +0530

    async await addition

--- src/Api.jsx
+++ src/Api.jsx
@@ -5,21 +5,58 @@ import axios from "axios";
 const Api = (props) =>{
 
     const [states,setState] = useState([]);
+
+    const initial= async () => {
+        let url = `http://localhost:8080/cat/allSubCat`;
+        let data ={
+            "categoryId":0
+        }
+      
+        try {
+          const rawResponse = await fetch(url, {
+            
+            method : 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+              },
+           
+            body: JSON.stringify(data),
+          });
+          if (rawResponse.ok) {
+            let response = await rawResponse.json();
+            console.log('From Method' +JSON.stringify(response))
+            setState((state) => ({...state, todos: response}))
+            alert("success");
+          } else {
+            const error = new Error();
+            error.message = "some error occured";
+            throw error;
+          }
+        } catch (e) {
+          // alert(`error: ${e.message}`);
+        }
+    }
    
-    useEffect(  () => {
-         fetch("https://jsonplaceholder.typicode.com/todos")
-        .then((res) => res.json())
-        .then((data) =>{
-            const five = data.slice(0,6)
-            setState((state) => ({...state, todos: five}));
-        });
+    useEffect(   () => {
+        //  fetch("http://localhost:8080/SubCat")
+        // .then((res) => res.json())
+        // .then((data) =>{
+        //     const five = data.slice(0,6)
+        //     setState((state) => ({...state, todos: five}));
+        // });
+        initial();
+        console.log(JSON.stringify(states));
+      
+    
     },[]);
     //console.log(props.todos);
     const renderTodos = () =>{
         if(typeof states.todos != 'undefined'){
             return states.todos.map(todo =>{
                 return (
-                <li key={todo.id}>{todo.title}</li>
+                <li key={todo.categoryId}>{todo.categoryName}</li>
             )})
         }
     }
